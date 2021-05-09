88a9c82aba31fe62c9affdaaa4fb4428d8d93563
marcos-roberto <marcosroberto.mr500@gmail.com>
2021-05-09 09:50:54 -0300

    mostrando próxima peça

--- Assets/Scripts/spawnTetro.cs
+++ Assets/Scripts/spawnTetro.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class spawnTetro : MonoBehaviour
 {
+    public int proxPeca;
+
     public Transform[] criaPecas;
+    public List<GameObject> mostraPecas; 
 
     // Start is called before the first frame update
     void Start()
     {
-        // intancia uma peça, localização: transforme.positiom; posição: padrão
-        Instantiate(criaPecas[Random.Range(0, 7)], transform.position, Quaternion.identity);
+        proxPeca = Random.Range(0, 7);
+        proximaPeca();
     }
 
     public void proximaPeca()
     {
-        Instantiate(criaPecas[Random.Range(0, 7)], transform.position, Quaternion.identity);
+        Instantiate(criaPecas[proxPeca], transform.position, Quaternion.identity);
+
+        proxPeca = Random.Range(0, 7);
+
+        for (int i = 0; i < mostraPecas.Count; i++)
+        {
+            mostraPecas[i].SetActive(false); // desabilita todas as peças
+        }
+
+        mostraPecas[proxPeca].SetActive(true); // mostra somente a próxima peça
     }
 }
